3e3ea2ac71480c7f16166c8ffbad19c1b0a7bddd
Shur03 <shuyeruult03@gmail.com>
2026-05-25 13:54:54 +0800

    Ignore Visual Studio workspace files (.vs)

---
+++ KioskSkytel/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+
+namespace KioskSkytel
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // optional: disable Alt+F4 (basic kiosk protection)
+            this.Closing += (s, e) =>
+            {
+                e.Cancel = true;
+            };
+        }
+
+        private void Services_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Services page coming soon");
+        }
+
+        private void Payments_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Payments page coming soon");
+        }
+
+        private void Balance_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Balance check coming soon");
+        }
+
+        private void Help_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Support page coming soon");
+        }
+    }
+}
